325a9bdd278ebaee36b9608d963e8fc96fa60371
Natthawut Haematulin <natthawut1991@gmail.com>
2023-11-23 22:45:30 +0700

    feat: implement http log interceptor

--- backend-master-class/simplebank/main.go
+++ backend-master-class/simplebank/main.go
@@ -137,7 +137,9 @@ func runGatewayServer(config util.Config, store db.Store) {
 	}
 
 	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
-	err = http.Serve(listener, mux)
+	// Use gapi.HttpLogger to log every request.
+	handler := gapi.HttpLogger(mux)
+	err = http.Serve(listener, handler)
 	if err != nil {
 		log.Fatal().Err(err).Msg("cannot start HTTP gateway server")
 	}
